2d73da76c3f0a64a1cc3a1be7d95234b8cf7a99b
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-02-22 17:30:13 +0300

    declare purchaseApi

---
+++ src/apis/purchase.api.ts
@@ -0,0 +1,16 @@
+import { Purchase, PurchaseListStatus } from 'src/types/purchases.type'
+import { SuccessResponseApi } from 'src/types/utils.type'
+import http from 'src/utils/http'
+
+const url = 'purchases'
+
+export const purchaseApi = {
+  addToCart(body: { product_id: string; buy_count: string }) {
+    return http.post<SuccessResponseApi<Purchase>>(`${url}/add-to-cart`, body)
+  },
+  getPurchases(params: { status: PurchaseListStatus }) {
+    return http.post<SuccessResponseApi<Purchase[]>>(`${url}`, {
+      params
+    })
+  }
+}

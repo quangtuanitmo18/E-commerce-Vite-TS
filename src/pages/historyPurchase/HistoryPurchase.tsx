0eac0555057f0141ce1ddb64f321962f7dc62615
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2025-04-13 23:43:32 +0300

    resolve err build

--- src/pages/historyPurchase/HistoryPurchase.tsx
+++
@@ -1,8 +0,0 @@
-const purchaseTabsLink = [
-  { status: purchaseStatus.all, name: 'All' },
-  { status: purchaseStatus.waitForConfirmation, name: 'To Pay' },
-  { status: purchaseStatus.waitForGetting, name: 'To Ship' },
-  { status: purchaseStatus.inProgress, name: 'To Receive' },
-  { status: purchaseStatus.deliverd, name: 'Completed' },
-  { status: purchaseStatus.cancelled, name: 'Cancelled' }
-]
